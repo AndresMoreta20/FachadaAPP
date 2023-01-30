980d49adf998c6c081dc7d553505c85aa7c566fa
AndresMoreta20 <andresmoreta2020@gmail.com>
2023-01-29 23:28:13 -0500

    correcion 2

--- src/components/PeliculaAPI.js
+++ src/components/PeliculaAPI.js
@@ -4,7 +4,7 @@ export const cargarPeliculas = async (nombre) => {
     let peliculas = "";
     const urlA = 'http://www.omdbapi.com/?t='
     // var nombre = 'Bladerunner'
-    const urlC = '&apikey=2ac64ca8'
+    const urlC = ''
     const nuevoURL = urlA + nombre + urlC
     fetch(nuevoURL)
         .then(res => {
@@ -19,21 +19,5 @@ export const cargarPeliculas = async (nombre) => {
         )
         .then(data => { return data })
         .catch(error => console.log(error));
-    /* const respuesta = await fetch(urlA)
-        .then(response => response.json())
-        .then(json => { peliculas = json })
-   // console.log(peliculas)
 
-    const pelicula = {
-        titulo: peliculas.Title.toString(),
-        anio: peliculas.Year.toString(),
-        duracion: peliculas.Runtime.toString(),
-        poster: peliculas.Poster.toString(),
-        genero: peliculas.Genre.toString(),
-        director: peliculas.Director.toString(),
-        pais: peliculas.Country.toString()
-    }
-    //console.log(pelicula)
-    return pelicula;
-    */
 }
